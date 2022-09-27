4185ff80c4ac640cd99b3c3d334548d2353c7030
AlexBosio95 <alex.bosio1995@gmail.com>
2022-09-27 16:58:28 +0200

    feat: jumbo and main with card

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,22 @@
 @extends('layout.app')
 
 @section('content')
-    @foreach ($discs as $disc)
-        {{ $disc['title'] }}
-    @endforeach
+
+<div class="ms_container mt-4">
+        <div class="container position-relative wrapper-cards">
+            <div class="cards-container">
+                    @foreach ($discs as $disc)
+                        <div class="ms_card">
+                            <img src="{{ $disc['thumb'] }}" alt="" srcset="">
+                            <h6 class="text-card">{{ $disc['title'] }}</h6>
+                        </div>
+                    @endforeach
+            </div>
+        </div>
+
+        <div class="text-center">
+            <button class="btn-more mb-3 fw-bold">LOAD MORE</button>
+        </div>
+</div>
+
 @endsection
---
+++ resources/views/partials/jumbo.blade.php
@@ -0,0 +1,10 @@
+<div class="ms_container mt-4">
+
+        
+        <img src="{{asset('images/jumbotron.jpg')}}" alt="">
+
+        <div class="container position-relative wrapper-cards">
+            <h5 class="fw-bold">CURRENT SERIES</h5>
+        </div>
+
+</div>
